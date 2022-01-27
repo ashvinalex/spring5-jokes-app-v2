f79144414649c494ef2c6520e4af756986acd05c
ashvinalex <ashvinalex@gmail.com>
2022-01-27 16:49:09 +0530

    added controller and view

---
+++ src/main/java/guru/spring/framework/spring5jokesappv2/controller/JokeController.java
@@ -0,0 +1,24 @@
+package guru.spring.framework.spring5jokesappv2.controller;
+
+import guru.spring.framework.spring5jokesappv2.services.JokesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokesService jokeService;
+
+    public JokeController(JokesService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
--- src/main/java/guru/spring/framework/spring5jokesappv2/services/JokesServiceImpl.java
+++ src/main/java/guru/spring/framework/spring5jokesappv2/services/JokesServiceImpl.java
@@ -5,15 +5,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class JokesServiceImpl implements JokesService {
+
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokesServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
-        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    public JokesServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
 
     @Override
     public String getJoke() {
-
-        return chuckNorrisQuotes.getRandomQuote() ;
+        return chuckNorrisQuotes.getRandomQuote();
     }
 }
